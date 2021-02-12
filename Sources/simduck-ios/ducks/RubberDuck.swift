8e522d755f38a49c8cf030919adb767d230f9489
carlos apablaza <c.apablaza.z@gmail.com>
2021-02-12 11:50:17 -0300

    override fly for rubber duck

--- Sources/simduck-ios/ducks/RubberDuck.swift
+++ Sources/simduck-ios/ducks/RubberDuck.swift
@@ -5,4 +5,8 @@ public class RubberDuck :  Duck{
     override public func display() {
         print("I'm RubberDuck!")
     }
+
+    override public func fly() {
+        print("I can't fly")
+    }
 }
